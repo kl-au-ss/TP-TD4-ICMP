61f11a5bffd3059bd1e875910bd7ba542cbfc280
Klaus <101809208+LucasRehmann@users.noreply.github.com>
2025-06-20 16:21:51 -0300

    Graficos

---
+++ graficos.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+universidades = ["UTDT", "MIT", "Cambridge", "UTS"]
+distancia = np.array([12, 8650, 11190, 11800])
+avgRTT = np.array([2.59, 4.43, 5.18, 5.67])
+maxRTTs = 
+
+## Grafico RTTs
+# fig, ax = plt.subplots()             # Create a figure containing a single Axes.
+# ax.plot(x, y)  # Plot some data on the Axes.
+# ax.yaxis.set_major_formatter
+# plt.xlabel("Distancia (en km)")
+# plt.ylabel("Avg. RTT")
+# plt.show()    
